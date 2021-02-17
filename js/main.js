3dad42514f76872a45c523c444d36d70dc79bd62
Croque <sdeing1@gmail.com>
2021-02-17 17:30:34 -0400

    Finishi

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+// require elements HTML
+
 const boton = document.getElementById("button");
 const input = document.getElementById("range");
 const view = document.getElementById("view");
 const pcd = document.getElementById("cost");
 
+// "input" event that is executed every time a change occurs in the input
 input.addEventListener("input", () => {
   let inputValue = input.value;
   if (inputValue === "1") {
+    // edit content HTML
     Reflect.set(view, "textContent", "10.000 views");
     Reflect.set(pcd, "textContent", "8$");
     console.log("cambios en el input: 1");
